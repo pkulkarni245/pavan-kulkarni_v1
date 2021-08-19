01a6a7aa4058968810e64f00924e0d535317f70d
pkulkarni245 <72600670+pkulkarni245@users.noreply.github.com>
2021-08-19 20:34:09 +0530

    Map Completed, Tooltip Position Adjustment Required

--- public/mapfunctions.js
+++ public/mapfunctions.js
@@ -1,17 +1,54 @@
 $(document).ready(function(){ 
     $(".pinpoint").click(function(){
-        var lang = ".lang-" + $(this).attr("id").substr(9,2);
-        $(lang).toggleClass("activated-lang");
-        if($("#pinpoint-en").hasClass("activated-lang")){
-            ($("#pinpoint-en").removeClass("activated-lang"));
-            ($("#pinpoint-en").addClass("activated-lang"));
-        }
+        var lang = $(this).attr("id").substr(9,2);
+        if(lang === "hi" || lang === "kn" || lang === "ma")
+            lang = "in";
+        $(".lang-" + lang).toggleClass("activated-" + lang);
     });
     $(".land").click(function(){
         if($(this).hasClass("lang-en")){
             $("#pinpoint-en").click();
         }
+        if($(this).hasClass("lang-fr")){
+            $("#pinpoint-fr").click();
+        }
+        if($(this).hasClass("lang-de")){
+            $("#pinpoint-de").click();
+        }
+        if($(this).hasClass("lang-es")){
+            $("#pinpoint-es").click();
+        }
+        if($(this).hasClass("lang-in")){
+            $("#pinpoint-hi").click();
+        }
     });
 
-    //TBD: what to do when click country with multiple language ON MAP... select/deselect which?
+
+    //Tooltip Functionality for Map to display Country
+    /*$(".land").hover(function(){
+        var ttText = $(this).attr("title");
+        var ttEle = $("#world-map-tooltip");
+        ttEle.text($(this).attr("title"));
+        var left = $(this).pageX - $(this).offset().left + 100;
+        var top = $(this).pageY - $(this).offset().top + 130;
+        ttEle.css("left", $(this).pageX);
+        ttEle.css("top", $(this).pageY);
+        ttEle.css("visibility","visible");
+
+    });*/
+    $(".land").mouseover(function(){
+        var ttText = $(this).attr("title");
+        var ttEle = $("#world-map-tooltip");
+        ttEle.text($(this).attr("title"));
+        /*var left = $(this).pageX - $(this).offset().left + 100;
+        var top = $(this).pageY - $(this).offset().top + 130;*/
+        ttEle.text(event.pageX + ", " + event.pageY);
+        ttEle.css("left", event.pageX);
+        ttEle.css("top", event.pageY);
+        ttEle.css("display","block");
+    });
+    $(".land").mouseout(function(){
+        var ttEle = $("#world-map-tooltip");
+        ttEle.css("display","none");
+    });
 });
