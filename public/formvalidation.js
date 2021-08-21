e908fa379b83933f0ba8db113e41e2d097c447bd
pkulkarni245 <72600670+pkulkarni245@users.noreply.github.com>
2021-08-21 23:17:19 +0530

    Form Linked with Firebase RTDB
    Form validation yet to be done

--- public/formvalidation.js
+++ public/formvalidation.js
@@ -1,5 +1,46 @@
 $(document).ready(function(){ 
-    $("#contact-form-submit").click(function(){
-        $("#contact-form").trigger("reset");
+    $("#contact-form").submit(function(evt){
+        /*
+        evt.preventDefault(); //Prevent the default form submit action
+        var uname = " ";
+        var uemail = " ";
+        var usub = " ";
+        var umsg = " ";
+        uname = $("#form-name").val();
+        uemail = $("#form-email").val();
+        usub = $("#form-sub").val();
+        umsg = $("#form-msg").val();
+        var emailregex = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
+        var othregex = /^[a-zA-Z][a-zA-Z\\s]+$/;
+        if(emailregex.test(uemail) != null && othregex.test(uname) != null && othregex.test(usub) !=null){
+            var formData = {
+                "name": uname,
+                "email": uemail,
+                "subject": usub,
+                "message": umsg,
+            }
+    
+                firebase.database().ref('/ContactFormResponses').push( formData );
+                $("#contact-form").trigger("reset");
+                $("#contact-form-submission-feedback").css("display","block");
+                $("contact-form-submission-feedback").text("Your response has been recorded successfully!");
+        }
+        else {
+            $("#contact-form-submission-feedback").css("display","block");
+            $("contact-form-submission-feedback").text("There are errors in your form. Please check and try again.");
+        }
+        //*/
+        var formData = {
+            "name": $('#form-name').val(),
+            "email": $('#form-email').val(),
+            "subject": $('#form-subject').val(),
+            "message": $('#form-message').val(),
+        }
+        evt.preventDefault(); //Prevent the default form submit action
+
+            firebase.database().ref('/ContactFormResponses').push( formData );
+            $("#contact-form").trigger("reset");
+            $("#contact-form-submission-feedback").css("display","block");
+            $("contact-form-submission-feedback").text("Your response has been recorded successfully!");
     });
 });
