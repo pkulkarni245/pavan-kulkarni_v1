22a86c7364db38f1ada866abf84f70e2d4829e0d
pkulkarni245 <72600670+pkulkarni245@users.noreply.github.com>
2021-09-11 20:01:55 +0530

    v1.4.5.7

---
+++ public/auth.js
@@ -0,0 +1,10 @@
+firebase.auth().signInAnonymously()
+.then(function() {
+   console.log('Logged in as Anonymous!')
+   
+   }).catch(function(error) {
+   var errorCode = error.code;
+   var errorMessage = error.message;
+   console.log(errorCode);
+   console.log(errorMessage);
+});
